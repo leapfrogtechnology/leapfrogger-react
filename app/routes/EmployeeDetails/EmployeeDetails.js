0b69fc0a58ba0f7ab9c2796202a7bcd667beea64
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-12-27 11:42:44 +0545

    employee-listing Create employee detail page

---
+++ app/routes/EmployeeDetails/EmployeeDetails.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {
+  Image,
+  Text,
+  View,
+  StyleSheet } from 'react-native';
+
+export default class EmployeeDetails extends Component{
+  constructor(props) {
+    super(props);
+  }
+
+  _separator(){
+      return(
+          <View style={{height:1, backgroundColor: 'lightgray', margin:5}}/>
+      );
+    }
+
+    render() {
+      return(
+        <View style={styles.container}>
+            <View style={{flex:1}}>
+          <Image style={styles.image} source={{uri: 'http://lorempixel.com/400/200'}}/>
+            </View>
+            <View style={{flex:2, padding:10}}>
+          <Text style={styles.title}>Achyut Pokhrel</Text>
+          {this._separator()}
+            </View>
+        </View>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    padding: 10,
+    paddingTop: 70,
+    flexDirection: 'row'
+  },
+  image: {
+    height: 200
+  },
+  title: {
+    fontSize: 25
+  },
+  subTitle:{
+    fontSize: 20
+  }
+});
