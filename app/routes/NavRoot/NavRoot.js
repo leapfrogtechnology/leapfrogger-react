c475f0a2361564394c8fe95767f5366fc64fd26c
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-12-29 11:16:39 +0545

    Change color for navigation header text

--- app/routes/NavRoot/NavRoot.js
+++ app/routes/NavRoot/NavRoot.js
@@ -1,89 +1,95 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Home from '../Home';
 import EmployeeDetails from '../EmployeeDetails';
 
 import {
-  BackAndroid,
-  NavigationExperimental
-} from 'react-native'
+    BackAndroid,
+    NavigationExperimental, Text
+} from 'react-native';
+
+import styles from './styles.js'
 
 const {
-  CardStack: NavigationCardStack,
-  Header: NavigationHeader
+    CardStack: NavigationCardStack,
+    Header: NavigationHeader
 } = NavigationExperimental
 
 export default class NavRoot extends Component {
-	constructor(props){
-		super(props);
-		this._renderScene = this._renderScene.bind(this);
-		this._handleBackAction = this._handleBackAction.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this._renderScene = this._renderScene.bind(this);
+        this._handleBackAction = this._handleBackAction.bind(this);
+    }
 
-	componentDidMount () {
-		BackAndroid.addEventListener('hardwareBackPress', this._handleBackAction)
-	}
-	componentWillUnmount () {
-		BackAndroid.removeEventListener('hardwareBackPress', this._handleBackAction)
-	}
+    componentDidMount() {
+        BackAndroid.addEventListener('hardwareBackPress', this._handleBackAction)
+    }
 
-	_renderScene(props) {
-		const { route } = props.scene;
-		if(route.key === 'home'){
-			return (
-				<Home _handleNavigate={this._handleNavigate.bind(this)} />
-			);
-		}
+    componentWillUnmount() {
+        BackAndroid.removeEventListener('hardwareBackPress', this._handleBackAction)
+    }
 
-		if(route.key === 'employeeDetails') {
-			return (
-				<EmployeeDetails _goBack={this._handleBackAction.bind(this)} employee={route.employee}/>
-			);
-		}
-	}
-	
-	_handleBackAction () {
-		if (this.props.navigation.index === 0) {
-			return false
-		}
-		this.props.popRoute()
-		return true
-	}
+    _renderScene(props) {
+        const {route} = props.scene;
+        if (route.key === 'home') {
+            return (
+                <Home _handleNavigate={this._handleNavigate.bind(this)}/>
+            );
+        }
 
-	_handleNavigate(action) {
-		switch (action && action.type) {
-			case 'push':
-				this.props.pushRoute(action.route)
-				return true;
+        if (route.key === 'employeeDetails') {
+            return (
+                <EmployeeDetails _goBack={this._handleBackAction.bind(this)} employee={route.employee}/>
+            );
+        }
+    }
 
-			case 'back':
-			case 'pop':
-				return this._handleBackAction()
-			default:
-				return false
-		}
-	}
+    _handleBackAction() {
+        if (this.props.navigation.index === 0) {
+            return false
+        }
+        this.props.popRoute()
+        return true
+    }
 
-	render () {
-    	return (
-			<NavigationCardStack
-				direction='vertical'
-				navigationState={this.props.navigation}
-				onNavigate={this._handleNavigate.bind(this)}
-				renderScene={this._renderScene}
-				renderHeader={props => (
-					<NavigationHeader
-						{...props}
-						onNavigateBack={this._handleBackAction}
-						renderTitleComponent={props => {
-							const title = props.scene.route.title
-							return <NavigationHeader.Title>{title}</NavigationHeader.Title>
-						}}
-					/>
-				)}
-			/>
-		)
-   }
+    _handleNavigate(action) {
+        switch (action && action.type) {
+            case 'push':
+                this.props.pushRoute(action.route)
+                return true;
 
+            case 'back':
+            case 'pop':
+                return this._handleBackAction()
+            default:
+                return false
+        }
+    }
+
+    render() {
+        return (
+            <NavigationCardStack
+                direction='vertical'
+                navigationState={this.props.navigation}
+                onNavigate={this._handleNavigate.bind(this)}
+                renderScene={this._renderScene}
+                renderHeader={props => (
+                    <NavigationHeader
+                        style={styles.navBar}
+                        {...props}
+                        onNavigateBack={this._handleBackAction}
+                        renderTitleComponent={props => {
+                            const title = props.scene.route.title
+                            return (
+                                <NavigationHeader.Title>
+                                    <Text style={styles.navBarTitle}>{title}</Text></NavigationHeader.Title>
+                            )
+                        }}
+                    />
+                )}
+            />
+        )
+    }
 
 
 }
---
+++ app/routes/NavRoot/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    navBar: {
+        backgroundColor: '#4CAF50',
+    },
+    navBarTitle: {
+        color: 'white'
+    }
+});
+
+export default styles;
