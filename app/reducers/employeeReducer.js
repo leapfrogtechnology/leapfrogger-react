762d4240afca72a7399865ef1a71f054561c8656
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2016-12-30 12:05:09 +0545

    remove console.log

--- app/reducers/employeeReducer.js
+++ app/reducers/employeeReducer.js
@@ -5,7 +5,6 @@ const initialState = {
 }
 
 function employeeState(state = initialState, action) {
-console.log('feri call vayo,', action)
 	switch(action.type) {
 		case LOADED_EMPLOYEES:
 			return Object.assign({}, state, {...state, employees: action.payload.employees, isLoading: action.payload.isLoading})
