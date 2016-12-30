af8fb393e14d9c4eedce872b24a2e466dd040ca8
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-12-30 11:10:56 +0545

    Add style to employee list

--- app/routes/Home/Home.js
+++ app/routes/Home/Home.js
@@ -1,76 +1,77 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   ListView,
   Text,
   View,
   TouchableHighlight,
   Image,
-  TextInput } from 'react-native';
+  TextInput
+} from 'react-native';
 
 import RestClient from '../../utils/RestClient';
-import { ENV } from '../../../environment';
-import { API } from '../../config/apis';
+import {ENV} from '../../../environment';
+import {API} from '../../config/apis';
 
 import styles from './styles.js';
 
-export default class Home extends Component{
+export default class Home extends Component {
 
-	constructor(props){
-		super(props);
-		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    	this.state = {
-      		employees: ds.cloneWithRows([])
-    	};
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      employees: ds.cloneWithRows([])
+    };
 
-    	RestClient.get(API.listEmployees, ENV.apiKey).then((data) => {
-    		this.setState({employees: ds.cloneWithRows(data)});
-    	});
-	}
+    RestClient.get(API.listEmployees, ENV.apiKey).then((data) => {
+      this.setState({employees: ds.cloneWithRows(data)});
+    });
+  }
 
-	_makePayload(employee) {
-		return {
-			type: 'push',
-			route: {
-				key: 'employeeDetails',
-				title: `${employee.firstName} ${employee.lastName}`,
-				employee: employee
-			}
-		}
-	}
+  _makePayload(employee) {
+    return {
+      type: 'push',
+      route: {
+        key: 'employeeDetails',
+        title: `${employee.firstName} ${employee.lastName}`,
+        employee: employee
+      }
+    }
+  }
 
-	_renderRow(employee) {
-		return (
-			<TouchableHighlight
-				underlayColor='#35b5ff'
-				onPress={() => this.props._handleNavigate(this._makePayload(employee))}
-			>
-	          <View style={styles.row}>
-	            <View style={{flex:3}}>
-	              <Image style={styles.image} source={{uri: employee.avatarUrl}} />
-	            </View>
-	            <View style={{flex:10, padding: 10}}>
-	              <Text style={styles.title}>{employee.firstName} {employee.lastName}</Text>
-	            </View>
-	            <View style={{flex:1, justifyContent: 'center'}}>
-	              <Text style={styles.title}> > </Text>
-	            </View>
-	          </View>
-	        </TouchableHighlight>
-		);
-	}
+  _renderRow(employee) {
+    return (
+      <TouchableHighlight
+        style={styles.employeeRowContainer}
+        underlayColor='grey'
+        onPress={() => this.props._handleNavigate(this._makePayload(employee))}>
+        <View style={styles.employeeRow}>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={{uri: employee.avatarUrl}}/>
+          </View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>{employee.firstName} {employee.lastName}</Text>
+          </View>
+          <View style={styles.favIconContainer}>
+            <Image style={styles.favIcon} source={require('../../images/ic_star_rate_black_18dp.png')}/>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  }
 
-	render() {
-		return (
-			<View style={{flex:1}}>
-				<View><TextInput placeholder='Search Employee' style={styles.searchBar}/></View>
-			   	<View style={{flex: 1}}>
-			   		<ListView style={styles.container}
-				      dataSource={this.state.employees}
-				      renderRow={(employee) => this._renderRow(employee)}
-				    />
-				</View>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View><TextInput placeholder='Search Employee' style={styles.searchBar}/></View>
+        <View style={{flex: 1}}>
+          <ListView style={styles.container}
+                    dataSource={this.state.employees}
+                    renderRow={(employee) => this._renderRow(employee)}
+          />
+        </View>
+      </View>
+    );
+  }
 
 } //end of class
--- app/routes/Home/styles.js
+++ app/routes/Home/styles.js
@@ -1,26 +1,44 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
-  row: {
+  employeeRowContainer: {
+    height: 48,
+    padding: 4,
     flexDirection: 'row',
-    height: 100,
-    margin: 5,
-    backgroundColor: 'rgba(0, 147, 209, 0.7)',
-    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  employeeRow: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 2
   },
   image: {
-    height: 100,
-    borderRadius: 5
+    height: 36,
+    width: 36,
+    borderRadius: 20
+  },
+  titleContainer: {
+    flex: 9
   },
   title: {
-    fontSize: 20,
+    fontSize: 16,
     color: '#222'
   },
   searchBar: {
     borderColor: 'rgba(0, 147, 209, 0.7)'
+  },
+  favIconContainer: {
+    flex: 1
+  },
+  favIcon: {
+    tintColor: '#AAA9A9',
   }
 });
 
