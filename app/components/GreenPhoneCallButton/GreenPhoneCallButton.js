3e3738df2869f26bd1b3929e5f863b0c2beb3357
Grishma Shrestha <grishmashrestha@lftechnology.com>
2017-01-04 15:31:02 +0545

    Create custom components to handle call/text to the provided home/mobile number for the employee

---
+++ app/components/GreenPhoneCallButton/GreenPhoneCallButton.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {Image, StyleSheet, TouchableOpacity} from 'react-native';
+import LinkingButton from '../LinkingButton/LinkingButton';
+
+
+export default class GreenPhoneCallButton extends LinkingButton {
+  constructor(props) {
+    super(props);
+    this.state = {linkType: 'tel:'};
+  }
+
+  render() {
+    if (this.props.url === null) {
+      return (
+        <Image
+          style={[styles.phoneImage, styles.contactRowImage]}
+          source={require('../../images/ic_phone_white_24dp.png')}/>
+      )
+    } else {
+      return (
+        <TouchableOpacity
+          onPress={this._handleClick.bind(this)}>
+          <Image
+            style={[styles.phoneImage, styles.contactRowImage]}
+            source={require('../../images/ic_phone_white_24dp.png')}/>
+        </TouchableOpacity>
+      )
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  phoneImage: {
+    height: 25,
+    width: 25,
+    tintColor: 'green'
+  },
+  contactRowImage: {
+    height: 20,
+    width: 20
+  }
+});
