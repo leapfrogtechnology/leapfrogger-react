60490cc59ff1200c4fa5710f9ff106d096dd666f
Kusal <shrstha.kusal@gmail.com>
2017-11-20 15:32:34 +0545

    master: project setup

---
+++ app/screens/contact/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+import colors from 'App/config/colors';
+
+const style = StyleSheet.create({
+  
+});
+
+export default style;
---
+++ app/screens/login/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { 
+  View, Text
+ } from 'react-native';
+import style from './styles';
+
+ class LoginScreen extends Component {
+
+ }
+
+ export default LoginScreen
