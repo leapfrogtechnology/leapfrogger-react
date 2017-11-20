551618c114e63cd9b745e1109e24800f5c6eca2d
Kusal <shrstha.kusal@gmail.com>
2017-11-20 12:52:28 +0545

    master: uri and color constants added

---
+++ app/config/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  LF_DARK_GRREEN: '#17a555',
+  LF_LIGHT_GREEN: '#6cc067',
+  LF_GRAY: '#6e6f72',
+
+};
+
+const TOUCH_OPACITY = 0.6;
+
+export { colors as default, TOUCH_OPACITY };
---
+++ app/config/uri.js
@@ -0,0 +1,6 @@
+export const BASE_URI = 'https://lms.lftechnology.com';
+
+export const uri = {
+};
+
+export default uri;
