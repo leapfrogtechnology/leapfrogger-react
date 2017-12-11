f824c5fdb5e1309c7391693eb8b1f5fae000fd7c
Kusal <shrstha.kusal@gmail.com>
2017-12-11 21:14:09 +0545

    todo

---
+++ app/screens/searchContact/searchContact.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { 
+  View,
+  Text,
+  Image,
+  FlatList
+ } from 'react-native';
+ 
+import { getWidth, getHeight } from 'App/utils/dimension';
+import style from './styles';
+ 
+ class SearchContactView extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  _renderTableView = () => {
+    return (
+      <FlatList
+        ref="ListView"
+        data={[{key: 'a'}, {key: 'b'}]}
+        renderItem={({item, section, index}) => this._renderItem() }
+      />
+    );
+  }
+
+  render() {
+    return (
+      <View style={ style.mainContainer }>
+        {
+          this._renderTableView()
+        }
+      </View>
+    );
+  }
+
+ }
+
+ export default SearchContactView
